1e17405821bb3de662abf4de3b1732d5410b5fd1
agusgatto8 <agustingatto8@gmail.com>
2022-11-17 22:56:00 -0300

    BaSP-2022-W14: Moving forward with the test

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,30 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
 
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
+    beforeAll('Navigate URL', () => {
+        browser.url('https://www.saucedemo.com/');
+    })
+    it('should not login with empty fields', async () => {
+        await LoginPage.login('', '');
+        await expect(LoginPage.errorMessage).toBeExisting();
+        await expect(LoginPage.errorMessage).toHaveTextContaining(
+            'Epic sadface: Username is required');
+    });
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+    it('should not login with empty username fields', async () => {
+        await LoginPage.login('standard_user', '');
+        await expect(LoginPage.errorMessage).toBeExisting();
+        await expect(LoginPage.errorMessage).toHaveTextContaining(
+            'Epic sadface: Password is required');
+    });
+
+    it('should not login with empty password fields', async () => {
+        await LoginPage.inputUsername.clearValue();
+        await browser.pause(10000)
+        await LoginPage.login('', 'secret_sauce');
+        await expect(LoginPage.errorMessage).toBeExisting();
+        await expect(LoginPage.errorMessage).toHaveTextContaining(
+            'Epic sadface: Username is required');
     });
 });
 
